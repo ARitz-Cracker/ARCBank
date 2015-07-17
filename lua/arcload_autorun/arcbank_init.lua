44f73b55e341b2513d1e4d179b7b09b3106a12cb
Aritz Beobide-Cardinal <aritz@aritzcracker.ca>
2015-07-17 09:31:32 -0400

    Next update?
    Signed-off-by: Aritz Beobide-Cardinal <aritz@aritzcracker.ca>

--- lua/arcload_autorun/arcbank_init.lua
+++ lua/arcload_autorun/arcbank_init.lua
@@ -81,8 +81,8 @@ ARCBankMsg("© Copyright 2014,2015 Aritz Beobide-Cardinal (ARitz Cracker) All ri
 
 ARCBank = ARCBank or {}
 
-ARCBank.Update = "June 8th 2015"
-ARCBank.Version = "1.3.2b"
+ARCBank.Update = "July 17th 2015"
+ARCBank.Version = "1.3.2c"
 
 ARCBank.About = [[      
              *** ARitz Cracker Bank ***
