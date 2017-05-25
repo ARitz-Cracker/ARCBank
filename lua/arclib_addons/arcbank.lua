bb33de3d1fb8643d88d362f4dd11adb3f68b81eb
Aritz Beobide-Cardinal <aritz@aritzcracker.ca>
2017-05-25 16:43:59 -0400

    change version number

--- lua/arclib_addons/arcbank.lua
+++ lua/arclib_addons/arcbank.lua
@@ -22,8 +22,8 @@ ARCBank.Msg("© Copyright 2014-2017 Aritz Beobide-Cardinal (ARitz Cracker) All r
 ARCBank.Features = {}
 ARCBank.Features["hackapi"] = true
 
-ARCBank.Update = "May 1st 2017"
-ARCBank.Version = "1.4.1d"
+ARCBank.Update = "May 25th 2017"
+ARCBank.Version = "1.4.1e"
 
 
 ARCBank.About = [[
